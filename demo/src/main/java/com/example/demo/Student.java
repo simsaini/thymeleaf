333a471f04c11c45ac937ab81f52d7686d57384b
Simran Saini <Simranstory101@gmail.com>
2017-09-12 19:15:30 -0400

    finished-worked with Parker

---
+++ demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+
+    Grade grade;
+
+    public Student(String firstName, String lastName, Grade grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+
+    public void setGrade(Grade grade) {
+        this.grade = grade;
+    }
+}
+
